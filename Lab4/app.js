09f07efc7e16564fb7c845b31b6580bfc2ff75d5
Kasuni_Madhushika <kasunimadhushika77@gmail.com>
2021-03-31 08:05:13 +0530

    Lab-4 answers..

---
+++ Lab4/app.js
@@ -0,0 +1,10 @@
+const os = require('os');
+
+console.log('Architecture' + os.arch());
+console.log('CPUs ' + os.cpus().length);
+console.log('os ' + os.platform());
+
+
+
+
+
---
+++ Lab4/lab2.js
@@ -0,0 +1,7 @@
+const fileName = __dirname + '/test.txt';
+const outFileName = __dirname + '/test-copy.txt';
+
+const readStream = fs.createReadStream(fileName);
+const writeStream = fs.createWriteStream(outFileName);
+
+readStream.pipe(writeStream);
